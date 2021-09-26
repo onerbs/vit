5a57cce3816d06dcd71c55bca562a9a3f4bcb048
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-09-26 15:41:17 +0000

    getter: update

--- getter/actions.v
+++ getter/actions.v
@@ -1,27 +1,27 @@
 module getter
 
 import uwu.paths
-import uwu
+import vit
 import os
 
 // clone will clone the specified repository.
 pub fn (self Getter) clone(url string) ? {
 	flags := self.branch_flag()
-	uwu.exec('git clone $flags $url $self.target') ?
+	vit.exec('clone $flags $url $self.target') ?
 }
 
 // bare_clone will clone the specified repository with depth=1.
 pub fn (self Getter) bare_clone(url string) ? {
 	flags := self.branch_flag()
-	uwu.exec('git clone --depth=1 --single-branch $flags $url $self.target') ?
+	vit.exec('clone --depth=1 --single-branch $flags $url $self.target') ?
 }
 
 // download will clone the repository and erase the `.git` directory.
 pub fn (self Getter) download(url string) ? {
 	self.bare_clone(url) ?
 	mut target := self.target
-	if target.len == 0 {
-		target = os.base(url.trim_suffix('.git'))
+	if target.len < 1 {
+		target = paths.simple(url)
 	}
 	dir := paths.from_here(target, '.git')
 	os.rmdir_all(dir) ?
--- getter/getter.v
+++ getter/getter.v
@@ -15,19 +15,23 @@ mut:
 pub fn new(cfg Getter) ?Getter {
 	if cfg.target.len > 0 && os.exists(cfg.target) {
 		mut target := str.quote(cfg.target)
-		target = uwu.colorize(target, term.bold)
+		target = term.colorize(term.bold, target)
 		ups.raise('cannot clone into $target: already exists') ?
 	}
-	target := str.safe_quote(os.real_path(cfg.target))
+	target := get_target(cfg.target)
 	return Getter{target, cfg.branch}
 }
 
 // set_target change the target directory for the cloned repository.
 pub fn (mut self Getter) set_target(target string) {
-	self.target = target
+	self.target = get_target(target)
 }
 
 // set_branch change the target branch of the cloned repository.
 pub fn (mut self Getter) set_branch(branch string) {
 	self.branch = branch
+}
+
+fn get_target(base string) string {
+	return str.safe_quote(os.real_path(base))
 }

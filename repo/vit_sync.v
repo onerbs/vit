9405cf071856cde1922214cb6c4d1a96abc371a1
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-09-28 19:52:48 +0000

    repo: update functions to fetch/pull/push branches

--- repo/vit_sync.v
+++ repo/vit_sync.v
@@ -1,71 +1,71 @@
 module repo
 
-// fetch fetch all changes from the remote.
-pub fn (self Remote) fetch(args ...string) ? {
-	self.exec('fetch ' + args.join(' ')) ?
+// fetch fetch the changes from the specified remote.
+pub fn (self Branch) fetch(remote string) ? {
+	self.repo.exec('fetch $remote $self.name') ?
 }
 
-// fetch_upstream fetch the changes to the remote.
-pub fn (self Remote) fetch_upstream() ? {
-	branch := self.get_branch() ?
-	self.fetch('--set-upstream', self.name, branch) ?
+// fetch fetch the changes to the current branch.
+pub fn (self Remote) fetch() ? {
+	self.repo.get_current_branch().fetch(self.name) ?
 }
 
-// fetch_current fetch the chages in the current branch from the remote.
-pub fn (self Remote) fetch_current() ? {
-	branch := self.get_branch() ?
-	self.fetch_branch(branch) ?
-}
-
-// fetch_branch fetch the changes from the specfied branch from the remote.
+// fetch_branch fetch the changes to the specified branch.
 pub fn (self Remote) fetch_branch(branch string) ? {
-	self.fetch(self.name, branch) ?
+	self.repo.get_branch(branch) ?.fetch(self.name) ?
+}
+
+// fetch_upstream fetch the changes from the specified remote
+// and set up the upstream to the branch in the remote.
+pub fn (self Remote) fetch_upstream() ? {
+	branch := self.repo.get_current_branch().name
+	self.repo.exec('fetch --set-upstream $self.name $branch') ?
 }
 
 // -------------------------------------
 
-// pull pull all changes from the remote.
-pub fn (self Remote) pull(args ...string) ? {
-	self.exec('pull ' + args.join(' ')) ?
+// pull pull the changes from the specified remote.
+pub fn (self Branch) pull(remote string) ? {
+	self.repo.exec('pull $remote $self.name') ?
 }
 
-// pull_upstream pull the changes to the remote.
-pub fn (self Remote) pull_upstream() ? {
-	branch := self.get_branch() ?
-	self.pull('--set-upstream', self.name, branch) ?
+// pull pull the changes to the current branch.
+pub fn (self Remote) pull() ? {
+	self.repo.get_current_branch().pull(self.name) ?
 }
 
-// pull_current pull the chages in the current branch from the remote.
-pub fn (self Remote) pull_current() ? {
-	branch := self.get_branch() ?
-	self.pull_branch(branch) ?
-}
-
-// pull_branch pull the changes from the specfied branch from the remote.
+// pull_branch pull the changes to the specified branch.
 pub fn (self Remote) pull_branch(branch string) ? {
-	self.pull(self.name, branch) ?
+	self.repo.get_branch(branch) ?.pull(self.name) ?
+}
+
+// pull_upstream pull the changes from the specified remote
+// and set up the upstream to the branch in the remote.
+pub fn (self Remote) pull_upstream() ? {
+	branch := self.repo.get_current_branch().name
+	self.repo.exec('pull --set-upstream $self.name $branch') ?
 }
 
 // -------------------------------------
 
-// push push the changes to the remote.
-pub fn (self Remote) push(args ...string) ? {
-	self.exec('push ' + args.join(' ')) ?
+// push push the changes to the specified remote.
+pub fn (self Branch) push(remote string) ? {
+	self.repo.exec('push $remote $self.name') ?
 }
 
-// push_upstream push the changes to the remote.
-pub fn (self Remote) push_upstream() ? {
-	branch := self.get_branch() ?
-	self.push('--set-upstream', self.name, branch) ?
+// push push the changes in the current branch.
+pub fn (self Remote) push() ? {
+	self.repo.get_current_branch().push(self.name) ?
 }
 
-// push_current push the chages in the current branch to the remote.
-pub fn (self Remote) push_current() ? {
-	branch := self.get_branch() ?
-	self.push_branch(branch) ?
-}
-
-// push_branch push the changes from the specfied branch to the remote.
+// push_branch push the changes in the specified branch.
 pub fn (self Remote) push_branch(branch string) ? {
-	self.push(self.name, branch) ?
+	self.repo.get_branch(branch) ?.push(self.name) ?
+}
+
+// push_upstream push the changes to the specified remote
+// and set up the upstream to the branch in the remote.
+pub fn (self Remote) push_upstream() ? {
+	branch := self.repo.get_current_branch().name
+	self.repo.exec('push --set-upstream $self.name $branch') ?
 }

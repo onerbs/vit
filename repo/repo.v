d829595035835d16afe84fe42216ed6e85bc48b8
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-09-28 00:45:39 +0000

    repo!: fix logic for loading and initializing repositories; add command execution functions

--- repo/repo.v
+++ repo/repo.v
@@ -1,26 +1,25 @@
 module repo
 
+import vit
 import uwu.ups
-import uwu
 import os
 
 struct Repository {
 	root string
 }
 
-// here use the repository in the current working directory.
-pub fn here() ?Repository {
-	return load(os.getwd())
+// load manage the repository in the current directory.
+pub fn load() ?Repository {
+	return load_at(os.getwd())
 }
 
-// load use the repository in the provided directory.
-pub fn load(base string) ?Repository {
-	if os.exists(os.join_path(base, '.git')) {
-		root := os.real_path(base)
-		return Repository{root}
+// load_at manage the repository in the provided directory.
+pub fn load_at(base string) ?Repository {
+	repo := repo_at(base)
+	if repo.root.len < 1 {
+		ups.invalid('git repository', base) ?
 	}
-	ups.invalid('git repository', base) ?
-	return none
+	return repo
 }
 
 // init initialize a new git repository in the current directory.
@@ -28,35 +27,36 @@ pub fn init() ?Repository {
 	return init_at(os.getwd())
 }
 
-// init_at initialize a new git repository in the specified directory.
-pub fn init_at(dir string) ?Repository {
-	if os.exists(os.join_path(dir, '.git')) {
-		return here()
+// init_at initialize a new git repository in the provided directory.
+pub fn init_at(base string) ?Repository {
+	if os.exists(base) {
+		repo := repo_at(base)
+		if repo.root.len > 0 {
+			return repo
+		}
+	} else {
+		os.mkdir_all(base) ?
 	}
-	root := os.real_path(dir)
+	root := os.real_path(base)
 	repo := Repository{root}
 	repo.exec('init') ?
 	return repo
 }
 
-// exec execute a git command.
-pub fn (self Repository) exec(cmd string) ?string {
-	return uwu.exec('git -C "$self.root" $cmd') or { return error_with_code(err.msg, err.code) }
+fn repo_at(base string) Repository {
+	mut root := ''
+	if os.exists(os.join_path(base, '.git')) {
+		root = os.real_path(base)
+	}
+	return Repository{root}
 }
 
-// get_author return the name of the repository author.
-pub fn (self Repository) get_author() ?string {
-	mut res := self.exec('config --get real.name') ?
-	if res == '' {
-		res = self.exec('config --get user.name') ?
-	}
-	if res == '' {
-		return none
-	}
-	return res
+// exec execute a git command or throw an error.
+pub fn (self Repository) exec(cmd string) ? {
+	return vit.exec('-C "$self.root" $cmd')
 }
 
-// status return the repository status.
-pub fn (self Repository) status() ?string {
-	return self.exec('status')
+// get execute a git command and return the output.
+pub fn (self Repository) get(cmd string) string {
+	return vit.get('-C "$self.root" $cmd')
 }

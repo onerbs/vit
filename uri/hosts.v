6fb910cf41f0787e1780663074fb3d2680d30620
Code Hangen <111701513+codehangen@users.noreply.github.com>
2022-11-26 01:29:50 -0300

    update hosts.v with gogs, gitea
    Hi all.
    
    I created a new URL with the (gogs, gitea) host in hosts.v.

--- uri/hosts.v
+++ uri/hosts.v
@@ -15,9 +15,14 @@ pub fn github(repo string) Host {
 	return new_host(name: 'github.com', repo: repo)
 }
 
-// sourcehut create a new URL maker with the SourceHut host.
-pub fn sourcehut(repo string) Host {
-	return new_host(name: 'sourcehut.org', repo: repo)
+// gitea create a new URL maker with the Gitea host.
+pub fn gitea(repo string) Host {
+	return new_host(name: 'gitea.io', repo: repo)
+}
+
+// gogs create a new URL maker with the Gogs host.
+pub fn gogs(repo string) Host {
+	return new_host(name: 'gogs.io', repo: repo)
 }
 
 // gitlab create a new URL maker with the GitLab host.
